a0ad05a33b0aa90925b7f9f305a5a2f741a53768
Benjamin <bkm52@cornell.edu>
2026-03-01 15:02:00 -0500

    implemented linked list cycle using brute force O(n) space complexity and optimized to O(1) space by using Floyd’s Tortoise & Hare method

---
+++ problems/linked_list_cycle.py
@@ -0,0 +1,45 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        #bruteforce method is by using a seen set to add node if we have seen it
+        #time and space complexity will be O(n)
+        # curr = head
+        # seen = set()
+        # while curr:
+        #     if curr in seen:
+        #         return True
+        #     else:
+        #         seen.add(curr)
+        #     curr = curr.next
+        # return False
+
+        #optimal solution by using slow and fast counter
+        #space complexity will be O(1) and time complexity will be O(n)
+        fast = slow = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow is fast:
+                return True
+        return False
+    
+if __name__ == "__main__":
+    node = Solution()
+    head = ListNode(3)
+    a = ListNode(2)
+    b = ListNode(0)
+    c = ListNode(-4)
+    head.next = a
+    a.next = b
+    b.next = c
+    c.next = a
+
+    print(node.hasCycle(head))
+
